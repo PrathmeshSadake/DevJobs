fc322f439823211c47d787aef510c743921a034a
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-11 23:13:40 +0530

    Added images

--- lib/helpers/job_json_converter.dart
+++ lib/helpers/job_json_converter.dart
@@ -1,21 +1,27 @@
+import 'package:DevJobs/helpers/date_formatter.dart';
 import 'package:DevJobs/models/job.dart';
 
 class JsonConverter {
   void jsonConverter(dynamic data, List<Job> list) {
-    list.add(
-      Job(
-        id: data['id'],
-        type: data['type'],
-        url: data['url'],
-        createdAt: data['created_at'],
-        company: data['company'],
-        companyUrl: data['company_url'],
-        location: data['location'],
-        title: data['title'],
-        description: data['description'],
-        howToApply: data['how_to_apply'],
-        companyLogo: data['company_logo'],
-      ),
-    );
+    if (data['company_logo'] != null &&
+        data['company_url'] != null &&
+        data['url'] != null)
+      list.add(
+        Job(
+          id: data['id'],
+          type: data['type'],
+          url: data['url'],
+          createdAt: DateFormatter().formatter(
+            data['created_at'],
+          ),
+          company: data['company'],
+          companyUrl: data['company_url'],
+          location: data['location'],
+          title: data['title'],
+          description: data['description'],
+          howToApply: data['how_to_apply'],
+          companyLogo: data['company_logo'],
+        ),
+      );
   }
 }
