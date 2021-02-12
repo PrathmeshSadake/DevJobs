93c64409da85f7564378269f77e7b4291354c28f
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-12 23:43:31 +0530

    Created models

---
+++ lib/models/article.dart
@@ -0,0 +1,14 @@
+class Article {
+  final String headline;
+  final String description;
+  final String source;
+  final String webUrl;
+  final String date;
+
+  Article(
+      {this.headline,
+      this.description,
+      this.source,
+      this.webUrl,
+      this.date});
+}
---
+++ lib/models/job.dart
@@ -0,0 +1,27 @@
+class Job {
+  final String id;
+  final String type;
+  final String url;
+  final String createdAt;
+  final String company;
+  final String companyUrl;
+  final String location;
+  final String title;
+  final String description;
+  final String howToApply;
+  final String companyLogo;
+
+  Job({
+    this.id,
+    this.type,
+    this.url,
+    this.createdAt,
+    this.company,
+    this.companyUrl,
+    this.location,
+    this.title,
+    this.description,
+    this.howToApply,
+    this.companyLogo,
+  });
+}
