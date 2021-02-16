c6a6ab45d00ca7e7cfcea85d06473a7be03b1922
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-16 22:53:04 +0530

    created seperate text styles

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextStyle categoryItemTextStyle = GoogleFonts.lato(
+  fontSize: 14,
+  letterSpacing: 1.5,
+  color: Colors.white,
+  height: 1.2,
+  fontWeight: FontWeight.w600,
+);
+
+final TextStyle jobCardCompanyNameStyle = GoogleFonts.lato(
+  fontSize: 18,
+  letterSpacing: 1,
+  height: 1.2,
+  color: Color(0xFF232F34),
+  fontWeight: FontWeight.w600,
+);
+
+final TextStyle jobCardTitleStyle = GoogleFonts.lato(
+  fontSize: 16,
+  letterSpacing: 1,
+  height: 1.2,
+  color: Color(0xFF4A6572),
+  fontWeight: FontWeight.w700,
+);
+final TextStyle jobCardLocationStyle = GoogleFonts.lato(
+  fontSize: 15,
+  letterSpacing: .75,
+  height: 1,
+  color: Colors.black45,
+  fontWeight: FontWeight.w500,
+);
+
+final TextStyle jobCardTypeStyle = GoogleFonts.lato(
+  fontSize: 14,
+  letterSpacing: .75,
+  height: 1,
+  color: Color(0xFF344955),
+  fontWeight: FontWeight.w600,
+);
