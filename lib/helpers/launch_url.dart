17d8477ddacf61b58c15978c034aee92590d79bd
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-16 22:52:48 +0530

    Created a url launching helper method

--- lib/helpers/launch_url.dart
+++ lib/helpers/launch_url.dart
@@ -1,11 +1,11 @@
 import 'package:url_launcher/url_launcher.dart';
 
-class LaunchUrl{
-  static void launchUrl(String url) async{
+class LaunchUrl {
+  static void launchUrl(String url) async {
     if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
-  }
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
