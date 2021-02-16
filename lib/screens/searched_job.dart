d5b24e82e1f0a2e022ed8c09135da5cb3aacd6b5
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-16 23:20:58 +0530

    Added spinkit loader

--- lib/screens/searched_job.dart
+++ lib/screens/searched_job.dart
@@ -1,10 +1,11 @@
-import 'package:DevJobs/models/job.dart';
-import 'package:DevJobs/providers/jobs.dart';
-import 'package:DevJobs/providers/news.dart';
-import 'package:DevJobs/widgets/job_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+
+import '../models/job.dart';
+import '../providers/jobs.dart';
+import '../widgets/job_card.dart';
 
 class SearchedJobScreen extends StatefulWidget {
   final String searchField;
@@ -36,15 +37,30 @@ class _SearchedJobScreenState extends State<SearchedJobScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Color(0xFFF9AA33)),
+        backgroundColor: Color(0xFF344955),
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'DEVJOBS',
+          style: GoogleFonts.bioRhyme(
+            fontSize: 23,
+            fontWeight: FontWeight.w900,
+            letterSpacing: 5,
+            color: Color(0xFFF9AA33),
+          ),
+        ),
+      ),
       body: _isLoading
           ? Center(
               child: SpinKitRotatingCircle(
-                color: Colors.white,
+                color: Color(0xFFF9AA33),
                 size: 50.0,
               ),
             )
           : Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
               child: Consumer<Jobs>(
                 builder: (ctx, jobs, child) {
                   List<Job> searchedJobs = jobs.searchedJobs;
